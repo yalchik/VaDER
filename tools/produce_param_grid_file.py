f5a4398ebd20a820bf16551928c1d5eba8a5e747
Ilya Yalchyk <yalchik.ilya@gmail.com>
2020-11-23 08:33:19 +0100

    added param grid for paper simulation

--- tools/produce_param_grid_file.py
+++ tools/produce_param_grid_file.py
@@ -11,10 +11,29 @@ PARAM_GRID = {
     "batch_size": [16],
     "alpha": [1.0],
     "n_epoch": [10],
-    "n_perm": [3],
-    "n_splits": [2]
+    "n_perm": [100],
+    "n_splits": [2],
+    "n_repeats": [1]
 }
 
+# paper simulation
+hidden_layer_pows = [0, 1, 2, 3, 4, 5, 6]
+hidden_layer_pows_of_2 = [2**p for p in hidden_layer_pows]
+n_hidden_cases = [(p,) for p in hidden_layer_pows_of_2]
+# (1), (2), (4), ..., (64), (1, 1), (1, 2), ..., (1, 64), ..., (64, 64)
+n_hidden_cases += list(itertools.product(hidden_layer_pows_of_2, hidden_layer_pows_of_2))
+
+PARAM_GRID = {
+    "k": list(range(2, 16)),
+    "n_hidden": n_hidden_cases,
+    "learning_rate": [0.0001, 0.001, 0.01, 0.1],
+    "batch_size": [16, 32, 64, 128],
+    "alpha": [1.0],
+    "n_epoch": [20],
+    "n_perm": [1000],
+    "n_splits": [2],
+    "n_repeats": [20]
+}
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
